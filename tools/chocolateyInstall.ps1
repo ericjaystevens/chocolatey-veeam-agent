a2b34a487e293aff9ea977a885f2977b541eb622
Eric Stevens <ejsteven@admnet.oakland.edu>
2017-02-06 21:06:59 -0500

    installs but throughs 1000 return code and error

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,36 +1,18 @@
-﻿Function Get-ExplorerProcessCount
-{
-  $process = Get-Process explorer -ErrorAction SilentlyContinue
-  $processCount = ($process | Measure-Object).Count
-  return $processCount
-}
+﻿$toolsDir   = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$packageName = 'VeeamEndpointBackup'
+$installerDir = Join-Path $toolsDir 'installer'
+$downloadURL = 'https://download5.veeam.com/VeeamEndpointBackup_1.5.0.306.zip'
+$installer = 'VeeamEndpointBackup_1.5.0.306.exe'
 
-$initialProcessCount = Get-ExplorerProcessCount
-Write-Warning "This installer is known to close the explorer process. This means `nyou may lose current work. `nIf it doesn't automatically restart explorer, type 'explorer' on the `ncommand shell to restart it."
 
-$versionMinusDots = "16.04".Replace(".","")
-
-$toolsDir   = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
-
-If (Get-ProcessorBits -eq 64){
-    $installer = "7z$($versionMinusDots)-x64.msi"
-}
-
-Else{
-    $installer = "7z$($versionMinusDots).msi"
-}
+Install-ChocolateyZipPackage $packageName -url $downloadURL -UnzipLocation $installerDir
 
 $packageArgs = @{
-	packageName   = '7zip.install'
-	fileType      = 'msi'
-	file          = "$toolsDir\installer\$installer"
-	silentArgs    = '/qn'
+	packageName   = $installer 
+	fileType      = 'exe'
+	file          = "$installerDir\$installer"
+	silentArgs    = '/silent'
 }
 
 Install-ChocolateyInstallPackage @packageArgs
 
-$finalProcessCount = Get-ExplorerProcessCount
-if($initialProcessCount -lt $finalProcessCount)
-{
-  Start-Process explorer.exe
-}
